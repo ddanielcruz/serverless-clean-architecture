df2a0758557415e1a4a5695c4a1cafc71a0e6373
Daniel Cruz <danielcunha54@gmail.com>
2024-01-27 17:46:26 -0300

    chore: add eslint

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -2,7 +2,7 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@/libs/api-gateway'
 import { formatJSONResponse } from '@/libs/api-gateway'
 import { middyfy } from '@/libs/lambda'
 
-import schema from './schema'
+import type schema from './schema'
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event,
--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,5 +1,6 @@
-import schema from './schema'
 import { handlerPath } from '@/libs/handler-resolver'
+
+import schema from './schema'
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
