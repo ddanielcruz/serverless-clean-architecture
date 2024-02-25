f252be73832fec28b0920ea7bb5db8b6beb4096c
Daniel Cruz <danielcunha54@gmail.com>
2024-02-25 18:35:50 -0300

    feat: implement RequestNoteTranscription use case

---
+++ src/domain/notes/services/request-note-transcription.spec.ts
@@ -0,0 +1,64 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { makeNote } from '@/test/factories/note-factory'
+import { InMemoryNotesRepository } from '@/test/repositories/in-memory-notes-repository'
+
+import { RequestNoteTranscription } from './request-note-transcription'
+import type { Transcriber } from '../protocols/transcriber'
+
+class TranscriberStub implements Transcriber {
+  async requestTranscription(): Promise<{ transcriptionId: string }> {
+    return {
+      transcriptionId: 'any-transcription-id',
+    }
+  }
+
+  async loadTranscription(): Promise<string> {
+    return 'any-transcription'
+  }
+}
+
+describe('RequestNoteTranscription', () => {
+  let sut: RequestNoteTranscription
+  let notesRepository: InMemoryNotesRepository
+  let transcriber: TranscriberStub
+  const note = makeNote()
+
+  beforeEach(() => {
+    notesRepository = new InMemoryNotesRepository()
+    transcriber = new TranscriberStub()
+    sut = new RequestNoteTranscription(notesRepository, transcriber)
+    notesRepository.items.push(note)
+  })
+
+  it('gets note by audio ID using NotesRepository', async () => {
+    const getByAudioIdSpy = vi.spyOn(notesRepository, 'getByAudioId')
+    await sut.execute({ audioId: note.audio.id })
+
+    expect(getByAudioIdSpy).toHaveBeenCalledWith(note.audio.id)
+  })
+
+  it('returns a ResourceNotFoundError if note is not found', async () => {
+    const response = await sut.execute({ audioId: new UniqueEntityId() })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('requests a transcription using Transcriber', async () => {
+    const requestTranscriptionSpy = vi.spyOn(
+      transcriber,
+      'requestTranscription',
+    )
+    await sut.execute({ audioId: note.audio.id })
+
+    expect(requestTranscriptionSpy).toHaveBeenCalledWith(note.audio)
+  })
+
+  it('returns transcription ID on success', async () => {
+    const response = await sut.execute({ audioId: note.audio.id })
+
+    assert(response.isRight())
+    expect(response.value.transcriptionId).toBe('any-transcription-id')
+  })
+})
--- src/domain/notes/services/request-note-transcription.ts
+++ src/domain/notes/services/request-note-transcription.ts
@@ -1,13 +1,38 @@
-import type { Either } from '@/core/either'
+import { left, type Either, right } from '@/core/either'
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 
-export type RequestNoteTranscriptionRequest = { filename: string }
+import type { Audio } from '../entities/audio'
+import type { Transcriber } from '../protocols/transcriber'
+import type { NotesRepository } from '../repositories/notes-repository'
 
-export type RequestNoteTranscriptionResponse = Either<unknown, unknown>
+export type RequestNoteTranscriptionRequest = { audioId: UniqueEntityId }
+
+export type RequestNoteTranscriptionResponse = Either<
+  ResourceNotFoundError,
+  { transcriptionId: string }
+>
 
 // Responsible for identifying the note to be transcribed after the audio was
 // successfully uploaded, and then request the transcriber service to transcribe it.
-export abstract class RequestNoteTranscription {
-  abstract execute(
+export class RequestNoteTranscription {
+  constructor(
+    private readonly notesRepository: NotesRepository,
+    private readonly transcriber: Transcriber,
+  ) {}
+
+  async execute(
     request: RequestNoteTranscriptionRequest,
-  ): Promise<RequestNoteTranscriptionResponse>
+  ): Promise<RequestNoteTranscriptionResponse> {
+    const note = await this.notesRepository.getByAudioId(request.audioId)
+    if (!note) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const audio = note.audio as Audio
+    const { transcriptionId } =
+      await this.transcriber.requestTranscription(audio)
+
+    return right({ transcriptionId })
+  }
 }
